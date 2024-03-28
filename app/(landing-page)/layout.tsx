036f861ed24593cd3ea710b05d26142b57ac53e1
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-03-28 11:26:56 -0400

    added landing page layout

---
+++ app/(landing-page)/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar } from "./componenets/navbar";
+import { Footer } from "./componenets/footer";
+
+export const LandingPageLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="flex items-center justify-center flex-col h-full">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPageLayout;
