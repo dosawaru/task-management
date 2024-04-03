67caf4cf9e5339df61c9a9c0ba5b9e91d4eecea9
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-04-03 18:39:19 -0400

    updated metadate description, icons, and tab title dynamically

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { auth } from "@clerk/nextjs";
 import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
+
+//Updates the tabs name with the current selected organization name
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
