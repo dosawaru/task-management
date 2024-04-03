075105ea9103d30307a2b7775dc162961f1f1f53
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-04-03 14:30:44 -0400

    updated database schema

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -1,11 +1,16 @@
 import {z} from "zod";
 
-// Defining the schema for create board with validation for title 
+// Defining the schema for create board with validation
 export const CreateBoard = z.object({
     title: z.string({ 
         required_error: "Title is Required", 
         invalid_type_error: "Title is Required",
-    }).min(3, {
+    }).min(3, { //requiring a minimum of 3 letter for title name
         message: "Minimum length of 3 letters!"
-     }),
+    }),
+     image: z.string({ 
+        required_error: "Image is Required", 
+        invalid_type_error: "Image is Required",
+    }),
+
 });
