3b82ec786c5912f585b68d10f10f604d5a1b0320
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-04-06 23:21:46 -0400

    added update card action

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,21 @@
+import {z} from "zod";
+
+// Defining the schema for update card with validation
+export const UpdateCard = z.object({
+    boardId: z.string(),
+    description: z.optional( 
+        z.string({ 
+            required_error: "Description is Required", 
+            invalid_type_error: "Description is Required",
+        }).min(3, { 
+            message: "Minimum length of 3 letters!"
+        })
+    ),
+    title: z.string({ 
+        required_error: "Title is Required", 
+        invalid_type_error: "Title is Required",
+    }).min(3, { 
+        message: "Minimum length of 3 letters!"
+    }),
+     id: z.string(),
+});
