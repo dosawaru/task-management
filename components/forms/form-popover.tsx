6343058e45602d72ced1e70f7416904f010c4f20
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-04-08 02:59:49 -0400

    sesolved problem preventing image saving for board creation

--- components/forms/form-popover.tsx
+++ components/forms/form-popover.tsx
@@ -1,7 +1,6 @@
 "use client";
-
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-
 import {
   Popover,
   PopoverContent,
@@ -9,12 +8,14 @@ import {
 } from "@/components/ui/popover";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
-
 import { toast } from "sonner";
-
 import { useAction } from "@/hooks/use-actions";
 import { createBoard } from "@/actions/create-board";
-import { FormImageSelect } from "./form-image-select";
+import { cn } from "@/lib/utils";
+import { Check } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useFormStatus } from "react-dom";
+import { defaultImages } from "@/public/images/images";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -25,15 +26,38 @@ interface FormPopoverProps {
 
 export const FormPopover = ({
   children,
-  side,
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
   const router = useRouter();
 
+  const [images, setImages] = useState<Array<Record<string, any>>>([
+    defaultImages,
+  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedImageID, setSelectedImageID] = useState(null);
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const { pending } = useFormStatus();
+  const [defaultValue, setDefaultValue] = useState("");
+
+  //fetching saved images *try using unsplash api instead
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        setImages(defaultImages);
+      } catch (errors) {
+        console.log(errors);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success("Board created!");
       router.push(`/board/${data.id}`);
     },
@@ -45,7 +69,7 @@ export const FormPopover = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
-    const image = formData.get("image") as string;
+    const image = defaultValue;
 
     execute({ title, image });
   };
@@ -57,14 +81,56 @@ export const FormPopover = ({
       <PopoverContent
         align={align}
         className="w-80 pt-3 bg-white"
-        side={side}
-        sideOffset={sideOffset}
+        side={"bottom"}
+        sideOffset={5}
       >
         <div className="text-sm text-center">Create Board</div>
         <form action={onSubmit}>
           <div className="space-y-4">
             {/* display image */}
-            <FormImageSelect id="image" errors={fieldErrors} />
+            <div className="relative">
+              <div className="grid grid-cols-3 gap-2 mb-2">
+                {/* reads defaultImages array */}
+                {images.map((defaultImages) => (
+                  <div
+                    key={defaultImages.id}
+                    className={cn(
+                      " cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
+                      pending && "opacity-50 hover:opacity-50 cursor-auto"
+                    )}
+                    onClick={() => {
+                      if (pending) return;
+                      setSelectedImageID(defaultImages.id);
+                      setDefaultValue(
+                        `${defaultImages.id}|${defaultImages.urls.thumb}|${defaultImages.urls.full}`
+                      );
+                    }}
+                  >
+                    {/* retrieve the id and url of the image that is selected */}
+                    <input
+                      type="radio"
+                      className="hidden"
+                      defaultChecked={selectedImageID === defaultImages.id}
+                      disabled={pending}
+                    />
+                    {/* display image */}
+                    <Image
+                      src={defaultImages?.urls?.thumb}
+                      alt="unsplash image"
+                      className="object-cover rounded"
+                      fill
+                      sizes="(max-width: 90px) 90px, 50px"
+                    />
+                    {/* added a check mark on the selected image */}
+                    {selectedImageID == defaultImages.id && (
+                      <div className="absolute h-full w-full bg-black opacity-50 rounded flex items-center justify-center">
+                        <Check className="h-4 w-4 text-white" />
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
             {/* display text field */}
             <FormInput
               id="title"
@@ -74,7 +140,7 @@ export const FormPopover = ({
             />
           </div>
           {/* submit button */}
-          <FormSubmit>Create</FormSubmit>
+          <FormSubmit className="w-full">Create</FormSubmit>
         </form>
       </PopoverContent>
     </Popover>
